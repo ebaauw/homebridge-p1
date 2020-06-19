5c0315072b276046da0c1c713719fae6d4c40e37
ebaauw <erik.baauw@xs4all.nl>
2020-06-19 22:41:36 +0200

    Update ctest.js

--- test/ctest.js
+++ test/ctest.js
@@ -24,12 +24,14 @@ async function main () {
   p1.on('error', (error) => {
     console.error('error: %s', errorToString(error))
   })
+  p1.on('connect', (url) => { console.log('connected to %s', url) })
+  p1.on('disconnect', (url) => { console.log('disconnected from %s', url) })
   p1.on('telegram', (telegram) => { console.log('telegram: %s', telegram) })
   p1.on('rawData', (data) => { console.log('rawData:', data) })
   p1.on('data', (data) => { console.log('data:', data) })
 
-  p1.connect('telegram')
-  p1.connect('rawData')
+  // p1.connect('telegram')
+  // p1.connect('rawData')
   p1.connect('data')
 }
 
